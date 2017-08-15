5b327ca4f04ca6c95c4c4c3e7c42ac0ab5682da1
Matt McDonald <mattmcd1221@gmail.com>
2017-08-15 19:52:16 -0400

    fix header fade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Headroom from 'react-headroom'
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.downTolerance = 500,
+    this.upTolerance = 1000
+  }
+
   render() {
     return (
+
       <div className="headerWrapper">
       <div className="Header">
         <h1>Matt McDonald</h1><p>Front-End Developer</p>
@@ -16,6 +24,7 @@ class Header extends Component {
         <a href="#Featured">Featured</a>
       </div>
       </div>
+  
     );
   }
 }
